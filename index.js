0ee6ff0e20a455a7c064f6bc7b87ba33cc223b69
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-01-20 23:36:22 +0500

    Add Node and Linked List

---
+++ index.js
@@ -0,0 +1,53 @@
+class Node {
+
+	data
+	pointer
+
+	constructor(data) {
+		this.data = data
+		this.pointer = null
+	}
+}
+
+
+class LinkedList {
+
+	head
+
+	constructor() {
+		this.head = null
+	}
+
+	insert(data) {
+
+		const node = new Node(data)
+		
+		if(!this.head) {
+			
+			this.head = node
+		
+		} else {
+			
+			let tail = this.head
+
+			while(tail.pointer) {
+
+				tail = tail.pointer
+
+			}
+
+			tail.pointer = node
+		
+		}
+	}
+
+}
+
+const ll = new LinkedList()
+
+ll.insert('A')
+ll.insert('B')
+ll.insert('C')
+ll.insert('D')
+
+console.log(ll.head.pointer)
