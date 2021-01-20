4b30e6b89cc30e101e7f13adc76bb18664f8dee3
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-01-21 00:04:29 +0500

    Add SingleLinkedList

---
+++ SingleLinkedList.js
@@ -0,0 +1,65 @@
+class Node {
+
+	data
+	pointer
+
+	constructor(data) {
+		this.data = data
+		this.pointer = null
+	}
+}
+
+
+class LinkedList {
+
+	head
+	#size = 0
+
+	constructor() {
+		this.head = null
+	}
+
+	get getSize() {
+		return this.#size
+	}
+
+	get displayNodes() {
+		let output = ''
+		let tail = this.head
+
+		while(tail.pointer) {
+			output += tail.data + '->'
+			tail = tail.pointer
+		}
+
+		return output
+	}
+
+	insert(data) {
+		const node = new Node(data)
+
+		if(!this.head) {
+			this.head = node
+		} else {
+			let tail = this.head
+
+			while(tail.pointer) {
+				tail = tail.pointer
+			}
+
+			tail.pointer = node
+		}
+
+		this.#size++
+	}
+
+	prepend(data) {
+		const newNode = new Node(data)
+
+		newNode.pointer = this.head
+		this.head = newNode
+		this.#size++
+	}
+}
+
+module.exports.LinkedList = LinkedList
