e698ae74afc80e23a5dd6e33277cbe4266c98284
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-01-20 23:55:18 +0500

    Add DisplayNodes

--- index.js
+++ index.js
@@ -13,32 +13,55 @@ class Node {
 class LinkedList {
 
 	head
+	#size = 0
 
 	constructor() {
 		this.head = null
 	}
 
-	insert(data) {
+	get getSize() {
+		return this.#size
+	}
 
+	get displayNodes() {
+		let output = ''
+		let tail = this.head
+
+		while(tail.pointer) {
+			output += tail.data + '->'
+			tail = tail.pointer
+		}
+
+		return output
+	}
+
+	insert(data) {
 		const node = new Node(data)
 
 		if(!this.head) {
-			
 			this.head = node
-		
 		} else {
-
 			let tail = this.head
 
 			while(tail.pointer) {
-
 				tail = tail.pointer
-
 			}
 
 			tail.pointer = node
 		}
+
+		this.#size++
 	}
+
+	prepend(data) {
+		const newNode = new Node(data)
+
+		newNode.pointer = this.head
+		this.head = newNode
+		this.#size++
+	}
+
+
 }
 
 const ll = new LinkedList()
@@ -48,4 +71,9 @@ ll.insert('B')
 ll.insert('C')
 ll.insert('D')
 
-console.log(ll)
+ll.prepend('F')
+ll.prepend('G')
+
+// console.log(ll)
+console.log(ll.getSize)
+console.log(ll.displayNodes)
